9a3f0bd0e85e35fa4c3da56ef111ad014b776356
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2022-02-18 10:31:46 +0100

    users list
    Co-Authored-By: barbossa2406 <75279988+barbossa2406@users.noreply.github.com>

---
+++ Andromeda/Entities/Comment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda
+{
+    internal class Comment
+    {
+        public int CommentID { get; set; }
+        public string CommentText { get; set; }
+        public DateTime CommentDate { get; set; }
+
+        public Comment() { }
+        public Comment(int ID , string text , DateTime date)
+        {
+            this.CommentID = ID;
+            this.CommentText = text;
+            this.CommentDate = date;
+
+        }
+
+        public static void importComments() { }
+        public static void exportComments() { }
+        public static void deleteComment() { }
+        public static void updateComment() { }
+    }
+
+    
+
+
+}
