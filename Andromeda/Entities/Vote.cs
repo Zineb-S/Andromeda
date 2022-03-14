4c40d2578de2c943d1afec4ad4a852a2d18451c1
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2022-03-14 08:58:36 +0100

    vote in search working

--- Andromeda/Entities/Vote.cs
+++ Andromeda/Entities/Vote.cs
@@ -47,11 +47,11 @@ namespace Andromeda.Entities
             MySqlDataAdapter adapter = new MySqlDataAdapter();
             DataTable table = new DataTable();
             MySqlCommand command = new MySqlCommand("SELECT " +
-                "Post_ID,"+
-                "count(*) VoteNumber,"+
-                "sum(Vote_Like) LikeNumber,"+
+                "Post_ID," +
+                "count(*) VoteNumber," +
+                "sum(Vote_Like) LikeNumber," +
                 "sum(Vote_Dislike) DislikeNumber," +
-               " SUM(if (User_ID = "+Program.CurrentUserID+", Vote_Like , 0)) as Like_Current_User ," +
+               " SUM(if (User_ID = " + Program.CurrentUserID + ", Vote_Like , 0)) as Like_Current_User ," +
                 "SUM(if (User_ID = " + Program.CurrentUserID + ", Vote_Dislike , 0)) as Dislike_Current_User" +
                 " FROM votes GROUP BY Post_ID; ", db.getConnection());
             adapter.SelectCommand = command;
@@ -71,7 +71,7 @@ namespace Andromeda.Entities
             Program.AllVotesList.Clear();
             ImportAllPostsVotes(Program.AllVotesList);
 
-            for (int i = 0; i < Program.AllVotesList.Count; i+=6)
+            for (int i = 0; i < Program.AllVotesList.Count; i += 6)
             {
                 if (Convert.ToInt32(Program.AllVotesList[i]).Equals(PostID))
                 {
@@ -88,78 +88,96 @@ namespace Andromeda.Entities
         }
         public static void exportPostVotes(string PostID, string VoteType)
         {
-            bool checkUserVoted(int UserID, string PoID)
-            {   Program.AllVotesList.Clear();
-                Program.VotesList.Clear();
-                ImportPostVotes(Convert.ToInt32(PostID),Program.VotesList);
-                if(Program.VotesList.Count > 0)
+            try
+            {
+                bool checkUserVoted(int UserID, string PoID)
                 {
-                    return true;
+                    Program.AllVotesList.Clear();
+                    Program.VotesList.Clear();
+                    ImportPostVotes(Convert.ToInt32(PostID), Program.VotesList);
+                    if (Program.VotesList.Count > 0)
+                    {
+                        for (int i = 0;i < Program.VotesList.Count;i+=6)
+                        {
+                            if (Convert.ToInt32(Program.VotesList[i]).Equals(PoID) & (Convert.ToInt32(Program.VotesList[i+4]).Equals(1) || Convert.ToInt32(Program.VotesList[i+5]).Equals(1)))
+                            {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                    return false;
+
+
+
+
+                }
+                // check if user voted on 0 0 
+                if (!checkUserVoted(Program.CurrentUserID, PostID))
+                {
+                    DB db = new DB();
+                    switch (VoteType)
+                    {
+                        case "Like":
+
+                            MySqlCommand command = new MySqlCommand("INSERT INTO votes(Post_ID,User_ID,Vote_Like,Vote_Dislike) VALUES(@postID, @userID, @VoteL,@VoteDL)", db.getConnection());
+                            command.Parameters.Add("@postID", MySqlDbType.Int32).Value = PostID;
+                            command.Parameters.Add("@userID", MySqlDbType.Int32).Value = Program.CurrentUserID;
+                            command.Parameters.Add("@VoteL", MySqlDbType.Int32).Value = 1;
+                            command.Parameters.Add("@VoteDL", MySqlDbType.Int32).Value = 0;
+                            db.openConnection();
+                            command.ExecuteNonQuery();
+                            db.closeConnection();
+                            break;
+
+                        case "Dislike":
+
+                            MySqlCommand command2 = new MySqlCommand("INSERT INTO votes(Post_ID,User_ID,Vote_Like,Vote_Dislike) VALUES(@postID, @userID, @VoteL,@VoteDL)", db.getConnection());
+                            command2.Parameters.Add("@postID", MySqlDbType.Int32).Value = PostID;
+                            command2.Parameters.Add("@userID", MySqlDbType.Int32).Value = Program.CurrentUserID;
+                            command2.Parameters.Add("@VoteL", MySqlDbType.Int32).Value = 0;
+                            command2.Parameters.Add("@VoteDL", MySqlDbType.Int32).Value = 1;
+                            db.openConnection();
+                            command2.ExecuteNonQuery();
+                            db.closeConnection();
+                            break;
+                    }
                 }
                 else
                 {
-                    return false;
-                }
+                    DB db = new DB();
+                    switch (VoteType)
+                    {
 
+                        case "Like":
+
+                            MySqlCommand command = new MySqlCommand("UPDATE  votes SET Vote_Like=1 , Vote_Dislike=0  WHERE Post_ID = " + PostID + " and User_ID= " + Program.CurrentUserID + "", db.getConnection());
+
+                            db.openConnection();
+                            command.ExecuteNonQuery();
+                            db.closeConnection();
+
+                            break;
+
+                        case "Dislike":
+
+                            MySqlCommand command2 = new MySqlCommand("UPDATE votes SET Vote_Like=0 , Vote_Dislike=1  WHERE Post_ID = " + PostID + " and User_ID= " + Program.CurrentUserID + "", db.getConnection());
+
+                            db.openConnection();
+                            command2.ExecuteNonQuery();
+                            db.closeConnection();
+                            break;
+                    }
+
+                }
             }
-            // check if user voted on 0 0 
-            if (!checkUserVoted(Program.CurrentUserID, PostID))
+
+
+            catch (Exception ex)
             {
-                DB db = new DB();
-                switch (VoteType)
-                {
-                    case "Like":
-
-                        MySqlCommand command = new MySqlCommand("INSERT INTO votes(Post_ID,User_ID,Vote_Like,Vote_Dislike) VALUES(@postID, @userID, @VoteL,@VoteDL)", db.getConnection());
-                        command.Parameters.Add("@postID", MySqlDbType.Int32).Value = PostID;
-                        command.Parameters.Add("@userID", MySqlDbType.Int32).Value = Program.CurrentUserID;
-                        command.Parameters.Add("@VoteL", MySqlDbType.Int32).Value = 1;
-                        command.Parameters.Add("@VoteDL", MySqlDbType.Int32).Value = 0;
-                        db.openConnection();
-                        command.ExecuteNonQuery();
-                        db.closeConnection();
-                        break;
-
-                    case "Dislike":
-
-                        MySqlCommand command2 = new MySqlCommand("INSERT INTO votes(Post_ID,User_ID,Vote_Like,Vote_Dislike) VALUES(@postID, @userID, @VoteL,@VoteDL)", db.getConnection());
-                        command2.Parameters.Add("@postID", MySqlDbType.Int32).Value = PostID;
-                        command2.Parameters.Add("@userID", MySqlDbType.Int32).Value = Program.CurrentUserID;
-                        command2.Parameters.Add("@VoteL", MySqlDbType.Int32).Value = 0;
-                        command2.Parameters.Add("@VoteDL", MySqlDbType.Int32).Value = 1;
-                        db.openConnection();
-                        command2.ExecuteNonQuery();
-                        db.closeConnection();
-                        break;
-                }
+                MessageBox.Show("error  "+ex.Message);
             }
-            else
-            {
-                DB db = new DB();
-                switch (VoteType)
-                {
 
-                    case "Like":
-
-                        MySqlCommand command = new MySqlCommand("UPDATE  votes SET Vote_Like=1 , Vote_Dislike=0  WHERE Post_ID = " + PostID + " and User_ID= " + Program.CurrentUserID + "", db.getConnection());
-                        Console.WriteLine("WE ARE HEREEEEE");
-                        db.openConnection();
-                        command.ExecuteNonQuery();
-                        db.closeConnection();
-
-                        break;
-
-                    case "Dislike":
-
-                        MySqlCommand command2 = new MySqlCommand("UPDATE votes SET Vote_Like=0 , Vote_Dislike=1  WHERE Post_ID = " + PostID + " and User_ID= " + Program.CurrentUserID + "", db.getConnection());
-                       
-                        db.openConnection();
-                        command2.ExecuteNonQuery();
-                        db.closeConnection();
-                        break;
-                }
-
-            }
         }
     }
 }
